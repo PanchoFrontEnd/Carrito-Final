ea813c63652229194e243abe383f220d417c0d21
PanchoFrontEnd <francisco.2a.insuco1dp@gmail.com>
2023-11-22 23:27:13 -0300

    se aplican documentos faltantes

---
+++ recomendacion.js
@@ -0,0 +1,27 @@
+const recomendacion = ["Refresco de coco mango", "Refresco de frutilla", "Refresco de limón"]
+
+const [a, b] = recomendacion
+
+let boton = document.getElementById("boton2")
+boton.onclick = () => {
+    recomendacion.length !== 0 && Swal.fire({
+        title: 'Productos recomendados!',
+        text: 'Refresco de coco mango, refresco de frutilla y refresco de limón. ¿Qué esperas para probrarlos?',
+    })
+}
+
+function productosRecomendacion() {
+    fetch('recomendacion.json')
+        .then(response => response.json())
+        .then(bebida => {
+            bebida.forEach(bebida =>{
+             document.getElementById("tarjeta2").innerHTML+=`  
+            <div class="card" style="width: 300px;">
+            <h3> ${bebida.title} </h3>  
+            <img src="${bebida.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h4>${bebida.price}</h4>
+            `
+            });
+        });
+}
